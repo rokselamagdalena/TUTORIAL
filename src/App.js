81c585391586d71daca7becfea6b2769e1e067f7
rokselamagdalena <rokselamagdalena@gmail.com>
2017-12-12 16:11:52 +0100

    JSX transformation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+//React is reposible for rendering everthing to thr DOM
 import './App.css';
 
 class App extends Component {
-  render() {
+  render() { //render sth to the screen
     return (
+
+      //this is JSX it will be transoiled to Javascript
       <div className="App">
        <h1>Hi, I'm a React App</h1>
       </div>
     );
+    //this is compiled into this code
+    //   return React.createElement('div', {className: 'App',}, React.createElement('h1',null, 'Hi, I\'m a React App' ))
   }
 }
 
