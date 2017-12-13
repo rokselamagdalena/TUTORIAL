ef9e9cd321d3392b2b2ff1d6694a704d154fe794
EE8-empirica <szalona5>
2017-12-13 18:32:14 +0100

    'update'

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ class App extends Component {
       persons: [
           {name: 'Max', age: 28},
           {name: 'Susi', age: 17}
-      ]
+      ],
+        otherState: 'some other value'
     }
 
     switchNameHandler = () => {
-        console.log('Was clicked!');
-        this.setState({persons: [0].name = ''});
+        console.log('Was clickedd!');
+        this.setState({
+            persons: [
+                {name: 'Max', age: 28},
+                {name: 'Susi', age: 27}
+        ]})
     }
 
   render() { //render sth to the screen
@@ -25,8 +30,12 @@ class App extends Component {
        <h1>Hi, I'm a React App</h1>
           <p>I hope to have fun</p>
           <button onClick={this.switchNameHandler}>Switch Name</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies is: Racing</Person>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+          <Person
+              name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler}
+          >My hobbies is: Racing</Person>
+          <Person
+              name={this.state.persons[1].name} age={this.state.persons[1].age}
+          />
       </div>
     );
     //this is compiled into this code
